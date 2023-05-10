6626058dfaf471dc41ced7a3f3a60fa200a2280f
rakeshkandhi <rakeshkandi12345@gmail.com>
2023-05-10 19:17:22 +0530

    init

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def get_requirements(file_path:str) -> List[str]:
 setup(
     name="mlproject",
     version = "0.0.1",
-    author = "Rakesh",
+    author = "Rakesh Kandhi",
     author_email = "rakeshkandhi1432@gmail.com",
     packages = find_packages(),
     install_requires = get_requirements("requirements.txt")
