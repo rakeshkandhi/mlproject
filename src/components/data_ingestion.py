b4380a2613c23acc65101cb92b342d90786d4a3c
rakeshkandhi <rakeshkandi12345@gmail.com>
2023-05-11 22:21:52 +0530

    init

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -61,4 +61,4 @@ if __name__ == "__main__":
     data_transformation = DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
     mt = ModelTrainer()
-    print(mt.initiate_model_trainer(train_arr,test_arr))
+    print("The Accuracy is {:.2f}%".format(mt.initiate_model_trainer(train_arr,test_arr)*100))
