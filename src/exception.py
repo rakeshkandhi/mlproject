9a520973b3b17d427c7a12c75c275e6468650311
rakeshkandhi <rakeshkandi12345@gmail.com>
2023-05-30 20:04:54 +0530

    init

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
 import src.logger
+import logging
 
 def error_message_details(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -20,9 +21,9 @@ class CustomException(Exception):
 # driver code
 
 
-# if __name__=="__main__":
-#     try:
-#         a=1/0
-#     except Exception as e: 
-#         logging.info("Divide by zero")
-#         raise CustomException(e,sys)
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e: 
+        logging.info("Divide by zero")
+        raise CustomException(e,sys)
