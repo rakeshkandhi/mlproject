b39d7161baa61f93859309179351e304d865e446
rakeshkandhi <rakeshkandi12345@gmail.com>
2023-06-11 10:11:20 +0530

    modified:   app.py 	modified:   templates/home.html

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
+
 app = Flask(__name__)
 
 # route for the home page
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/predictdata",methods=['GET','POST'])
 def predict_datapoint():
@@ -27,14 +29,12 @@ def predict_datapoint():
         pred_df=data.get_data_as_data_frame()
         print(pred_df)
         print("Before Prediction")
-
         predict_pipeline=PredictPipeline()
         print("Mid Prediction")
         results=predict_pipeline.predict(pred_df)
         print("after Prediction")
         return render_template('home.html',results=results[0])
 
-
-
+        
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080,debug= True)
